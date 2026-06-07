e22db150c0468779058103e1e78ea5aeb0b7a187
SaiPrasanna275 <prasannadakuri123@gmail.com>
2026-06-07 11:45:55 +0530

    Time: 5 ms (78.63%) | Memory: 46.1 MB (17.20%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+      int sum=0,rem,temp=x;
+      while(temp>0){
+        rem=temp%10;
+        sum=(sum*10)+rem;
+        temp=temp/10;
+      }
+      if(x==sum){
+        return true;
+      }
+      return false;
+    }
+}
